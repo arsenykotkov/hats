551d9f976f7375580f92320fbd31b85b106d50a5
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-19 03:39:44 +1000

    Fix top menu to work in mobile mode.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
+import { useState } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Menu } from 'antd';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import News from './News';
 import About from './About';
 import Model from './Model';
 import Contacts from './Contacts';
-import menuItems from './menuItems';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <Menu items={menuItems} mode="horizontal" />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/news' element={<News />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/model' element={<Model />} />
-        <Route path='/contacts' element={<Contacts />} />
-      </Routes>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='news' element={<News />} />
+        <Route path='about' element={<About />} />
+        <Route path='model' element={<Model />} />
+        <Route path='contacts' element={<Contacts />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout(args) {
+  return (
+      <div style={{
+        height: '46px',
+        width: '100%',
+      }}>
+
+      <Navbar>
+        <Nav>
+          <NavLink>
+              <Link to="/">Home</Link>
+          </NavLink>
+          <NavLink>
+            <Link to="/news">News</Link>
+          </NavLink>
+          <NavLink>
+            <Link to="/about">About</Link>
+          </NavLink>
+          <NavLink>
+            <Link to="/model">Model</Link>
+          </NavLink>
+          <NavLink>
+            <Link to="/contacts">Contacts</Link>
+          </NavLink>
+        </Nav>
+      </Navbar>
+
+      <Outlet />
     </div>
   );
 }
--- src/Model.js
+++ src/Model.js
@@ -10,96 +10,112 @@ const { Panel } = Collapse;
 const { Sider, Content } = Layout;
 
 export default function Model() {
-const [crownColor, setCrownColor] = useState('#FFFFFF');
-const [capeColor, setCapeColor] = useState('#FFFFFF');
-const [visorColor, setVisorColor] = useState('#FFFFFF');
-const [topPointColor, setTopPointColor] = useState('#FFFFFF');
+    const [autoRotationEnabled, setAutoRotationEnabled] = useState(true);
 
-const [crownVisibility, setCrownVisibility] = useState(true);
-const [capeVisibility, setCapeVisibility] = useState(true);
-const [visorVisibility, setVisorVisibility] = useState(true);
-const [topPointVisibility, setTopPointVisibility] = useState(true);
+    const [crownColor, setCrownColor] = useState('#FFFFFF');
+    const [capeColor, setCapeColor] = useState('#FFFFFF');
+    const [visorColor, setVisorColor] = useState('#FFFFFF');
+    const [topPointColor, setTopPointColor] = useState('#FFFFFF');
 
-return (
-    <>
-        {/* <h1>Model</h1> */}
-        <Layout hasSider>
-            <Content>
-                <Canvas
-                    style={{
-                        boxShadow: 'inset 15px 15px 150px 5px #CCCCCC',
+    const [crownVisibility, setCrownVisibility] = useState(true);
+    const [capeVisibility, setCapeVisibility] = useState(true);
+    const [visorVisibility, setVisorVisibility] = useState(true);
+    const [topPointVisibility, setTopPointVisibility] = useState(true);
+
+    return (
+        <>
+            <Layout hasSider>
+                <Content>
+                    <Canvas
+                        style={{
+                            boxShadow: 'inset 15px 15px 150px 5px #CCCCCC',
+                            position: 'fixed',
+                            height: 'calc(100vh - 46px)',
+                            width: 'calc(100% - 320px)',
+                            right: '0',
+                            bottom: '0',
+                        }}
+                    >
+                        <OrbitControls
+                            autoRotate={autoRotationEnabled}
+                            autoRotateSpeed={2}
+                        />
+                        <ambientLight />
+                        <spotLight
+                            intensity={0.9}
+                            position={[10, 15, 10]}
+                            penumbra={1}
+                            angle={0.1}
+                            castShadow
+                        />
+                        <pointLight position={[-10, -10, -10]} />
+                        <Hat
+                            colors={{
+                                crownColor,
+                                capeColor,
+                                visorColor,
+                                topPointColor,
+                            }}
+                            visibility={{
+                                crownVisibility,
+                                capeVisibility,
+                                visorVisibility,
+                                topPointVisibility,
+                            }}
+                        />
+                    </Canvas>
+                    <Button
+                        onClick={() => setAutoRotationEnabled(autoRotationEnabled ? false : true)}
+                        type={autoRotationEnabled ? 'default' : 'primary'}
+                        style={{
                         position: 'fixed',
+                        bottom: '40px',
+                        left: 'calc(50% + 160px)',
+                        transform: 'translateX(-50%)'
+                        }}
+                    >
+                        {autoRotationEnabled ? 'Остановить' : 'Вращать'}
+                    </Button>
+                </Content>
+                <Sider
+                    width={320}
+                    style={{
+                        background: '#FFFFFF',
+                        overflow: 'auto',
                         height: 'calc(100vh - 46px)',
-                        width: 'calc(100% - 320px)',
-                        right: '0',
-                        top: '46'
-                    }}
-                >
-                    <OrbitControls />
-                    <ambientLight />
-                    <spotLight
-                        intensity={0.9}
-                        position={[10, 15, 10]}
-                        penumbra={1}
-                        angle={0.1}
-                        castShadow
-                    />
-                    <pointLight position={[-10, -10, -10]} />
-                    <Hat
-                        colors={{
-                            crownColor,
-                            capeColor,
-                            visorColor,
-                            topPointColor,
-                        }}
-                        visibility={{
-                            crownVisibility,
-                            capeVisibility,
-                            visorVisibility,
-                            topPointVisibility,
-                        }}
-                    />
-                </Canvas>
-            </Content>
-            <Sider
-                width={320}
-                style={{
-                    background: '#FFFFFF',
-                    overflow: 'auto',
-                    height: 'calc(100vh - 46px)',
-                    position: 'fixed',
-                    left: 0,
-                    top: 46,
-                    bottom: 0,
-                }}>
-                <Collapse className='model-menu' accordion>
-                    <Panel header="Стенка">
-                        <CirclePicker width="auto" onChange={color => setCrownColor(color.hex)} />
-                        <Button type={crownVisibility ? 'default' : 'primary'} onClick={() => setCrownVisibility(crownVisibility ? false : true)}>
-                            {crownVisibility ? 'Скрыть' : 'Показать'}
-                        </Button>
-                    </Panel>
-                    <Panel header="Отлетная часть">
-                        <CirclePicker width="auto" onChange={color => setCapeColor(color.hex)} />
-                        <Button type={capeVisibility ? 'default' : 'primary'} onClick={() => setCapeVisibility(capeVisibility ? false : true)}>
-                            {capeVisibility ? 'Скрыть' : 'Показать'}
-                        </Button>
-                    </Panel>
-                    <Panel header="Козырёк">
-                        <CirclePicker width="auto" onChange={color => setVisorColor(color.hex)} />
-                        <Button type={visorVisibility ? 'default' : 'primary'} onClick={() => setVisorVisibility(visorVisibility ? false : true)}>
-                            {visorVisibility ? 'Скрыть' : 'Показать'}
-                        </Button>
-                    </Panel>
-                    <Panel header="Верхняя заклёпка">
-                        <CirclePicker width="auto" onChange={color => setTopPointColor(color.hex)} />
-                        <Button type={topPointVisibility ? 'default' : 'primary'} onClick={() => setTopPointVisibility(topPointVisibility ? false : true)}>
-                            {topPointVisibility ? 'Скрыть' : 'Показать'}
-                        </Button>
-                    </Panel>
-                </Collapse>
-            </Sider>
-        </Layout>
-    </>
-);
+                        position: 'fixed',
+                        left: 0,
+                        top: 46,
+                        bottom: 0,
+                    }}>
+                    <Collapse className='model-menu' accordion>
+                        <Panel header="Стенка">
+                            <CirclePicker width="auto" onChange={color => setCrownColor(color.hex)} />
+                            <Button type={crownVisibility ? 'default' : 'primary'} onClick={() => setCrownVisibility(crownVisibility ? false : true)}>
+                                {crownVisibility ? 'Скрыть' : 'Показать'}
+                            </Button>
+                        </Panel>
+                        <Panel header="Отлетная часть">
+                            <CirclePicker width="auto" onChange={color => setCapeColor(color.hex)} />
+                            <Button type={capeVisibility ? 'default' : 'primary'} onClick={() => setCapeVisibility(capeVisibility ? false : true)}>
+                                {capeVisibility ? 'Скрыть' : 'Показать'}
+                            </Button>
+                        </Panel>
+                        <Panel header="Козырёк">
+                            <CirclePicker width="auto" onChange={color => setVisorColor(color.hex)} />
+                            <Button type={visorVisibility ? 'default' : 'primary'} onClick={() => setVisorVisibility(visorVisibility ? false : true)}>
+                                {visorVisibility ? 'Скрыть' : 'Показать'}
+                            </Button>
+                        </Panel>
+                        <Panel header="Верхняя заклёпка">
+                            <CirclePicker width="auto" onChange={color => setTopPointColor(color.hex)} />
+                            <Button type={topPointVisibility ? 'default' : 'primary'} onClick={() => setTopPointVisibility(topPointVisibility ? false : true)}>
+                                {topPointVisibility ? 'Скрыть' : 'Показать'}
+                            </Button>
+                        </Panel>
+                    </Collapse>
+                </Sider>
+            </Layout>
+        </>
+    );
 }
