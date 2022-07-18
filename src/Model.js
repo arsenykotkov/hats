e17dec921704f7470755cfcc083046d208569967
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-18 23:33:50 +1000

    Add color and visibility states for the hat.
    Hat changes its parts' color and visibility through props.
    Also add OrbitControls from @react-three/drei.

--- src/Model.js
+++ src/Model.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
 import Hat from './Hat';
 
 export default function Model() {
-    return (
-        <>
-            <h1>Model</h1>
-            <Canvas>
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <pointLight position={[-10, -10, -10]} />
-                <Hat />
-            </Canvas>
-        </>
-    );
+const [crownColor, setCrownColor] = useState('#FF0000');
+const [capeColor, setCapeColor] = useState('#FFFFFF');
+const [visorColor, setVisorColor] = useState('#FFFFFF');
+const [topPoint, setTopPointColor] = useState('#FFFFFF');
+
+const [crownVisibility, setCrownVisibility] = useState(true);
+const [capeVisibility, setCapeVisibility] = useState(true);
+const [visorVisibility, setVisorVisibility] = useState(true);
+const [topPointVisibility, setTopPointVisibility] = useState(true);
+
+return (
+    <>
+        <h1>Model</h1>
+        <Canvas>
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Hat
+                colors={{
+                    crownColor,
+                    capeColor,
+                    visorColor,
+                    topPoint,
+                }}
+                visibility={{
+                    crownVisibility,
+                    capeVisibility,
+                    visorVisibility,
+                    topPointVisibility,
+                }}
+            />
+        </Canvas>
+    </>
+);
 }
