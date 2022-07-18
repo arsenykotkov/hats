8b37e29e3021bb3b4da4de4f311717f221e238fa
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-19 01:16:31 +1000

    Add color change option.
    Also add antd Layout.

--- src/Hat.js
+++ src/Hat.js
@@ -13,7 +13,7 @@ export default function Hat({ ...props }) {
       <mesh geometry={nodes.Crown.geometry} material={materials['Material.001']} material-color={props.colors.crownColor} visible={props.visibility.crownVisibility} />
       <mesh geometry={nodes.Cape.geometry} material={materials['Material.004']} material-color={props.colors.capeColor} visible={props.visibility.capeVisibility}/>
       <mesh geometry={nodes.Visor.geometry} material={materials['Material.002']} material-color={props.colors.visorColor} visible={props.visibility.visorVisibility}/>
-      <mesh geometry={nodes.Top_point.geometry} material={materials['Material.003']} material-color={props.colors.topPointColor} visible={props.visibility.topPointVisibility}position={[0, 0.93, 0]} scale={[0.04, 0.02, 0.04]} />
+      <mesh geometry={nodes.Top_point.geometry} material={materials['Material.003']} material-color={props.colors.topPointColor} visible={props.visibility.topPointVisibility} position={[0, 0.93, 0]} scale={[0.04, 0.02, 0.04]} />
     </group>
   )
 }
--- src/Model.js
+++ src/Model.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
-import { Collapse, Button } from 'antd';
+import { Collapse, Button, Layout } from 'antd';
+import { CirclePicker } from "react-color";
+import './Model.css'
 import Hat from './Hat';
 
 const { Panel } = Collapse;
+const { Sider, Content } = Layout;
 
 export default function Model() {
-const [crownColor, setCrownColor] = useState('#FF0000');
+const [crownColor, setCrownColor] = useState('#FFFFFF');
 const [capeColor, setCapeColor] = useState('#FFFFFF');
 const [visorColor, setVisorColor] = useState('#FFFFFF');
-const [topPoint, setTopPointColor] = useState('#FFFFFF');
+const [topPointColor, setTopPointColor] = useState('#FFFFFF');
 
 const [crownVisibility, setCrownVisibility] = useState(true);
 const [capeVisibility, setCapeVisibility] = useState(true);
@@ -19,49 +22,84 @@ const [topPointVisibility, setTopPointVisibility] = useState(true);
 
 return (
     <>
-        <h1>Model</h1>
-        <Collapse>
-            <Panel header="Стенка">
-                <Button type={crownVisibility ? 'default' : 'primary'} onClick={() => setCrownVisibility(crownVisibility ? false : true)}>
-                    {crownVisibility ? 'Скрыть' : 'Показать'}
-                </Button>
-            </Panel>
-            <Panel header="Отлетная часть">
-                <Button type={capeVisibility ? 'default' : 'primary'} onClick={() => setCapeVisibility(capeVisibility ? false : true)}>
-                    {capeVisibility ? 'Скрыть' : 'Показать'}
-                </Button>
-            </Panel>
-            <Panel header="Козырёк">
-                <Button type={visorVisibility ? 'default' : 'primary'} onClick={() => setVisorVisibility(visorVisibility ? false : true)}>
-                    {visorVisibility ? 'Скрыть' : 'Показать'}
-                </Button>
-            </Panel>
-            <Panel header="Верхняя заклёпка">
-                <Button type={topPointVisibility ? 'default' : 'primary'} onClick={() => setTopPointVisibility(topPointVisibility ? false : true)}>
-                    {topPointVisibility ? 'Скрыть' : 'Показать'}
-                </Button>
-            </Panel>
-        </Collapse>
-        <Canvas>
-            <OrbitControls />
-            <ambientLight intensity={0.5} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <pointLight position={[-10, -10, -10]} />
-            <Hat
-                colors={{
-                    crownColor,
-                    capeColor,
-                    visorColor,
-                    topPoint,
-                }}
-                visibility={{
-                    crownVisibility,
-                    capeVisibility,
-                    visorVisibility,
-                    topPointVisibility,
-                }}
-            />
-        </Canvas>
+        {/* <h1>Model</h1> */}
+        <Layout hasSider>
+            <Content>
+                <Canvas
+                    style={{
+                        boxShadow: 'inset 15px 15px 150px 5px #CCCCCC',
+                        position: 'fixed',
+                        height: 'calc(100vh - 46px)',
+                        width: 'calc(100% - 320px)',
+                        right: '0',
+                        top: '46'
+                    }}
+                >
+                    <OrbitControls />
+                    <ambientLight />
+                    <spotLight
+                        intensity={0.9}
+                        position={[10, 15, 10]}
+                        penumbra={1}
+                        angle={0.1}
+                        castShadow
+                    />
+                    <pointLight position={[-10, -10, -10]} />
+                    <Hat
+                        colors={{
+                            crownColor,
+                            capeColor,
+                            visorColor,
+                            topPointColor,
+                        }}
+                        visibility={{
+                            crownVisibility,
+                            capeVisibility,
+                            visorVisibility,
+                            topPointVisibility,
+                        }}
+                    />
+                </Canvas>
+            </Content>
+            <Sider
+                width={320}
+                style={{
+                    background: '#FFFFFF',
+                    overflow: 'auto',
+                    height: 'calc(100vh - 46px)',
+                    position: 'fixed',
+                    left: 0,
+                    top: 46,
+                    bottom: 0,
+                }}>
+                <Collapse className='model-menu' accordion>
+                    <Panel header="Стенка">
+                        <CirclePicker width="auto" onChange={color => setCrownColor(color.hex)} />
+                        <Button type={crownVisibility ? 'default' : 'primary'} onClick={() => setCrownVisibility(crownVisibility ? false : true)}>
+                            {crownVisibility ? 'Скрыть' : 'Показать'}
+                        </Button>
+                    </Panel>
+                    <Panel header="Отлетная часть">
+                        <CirclePicker width="auto" onChange={color => setCapeColor(color.hex)} />
+                        <Button type={capeVisibility ? 'default' : 'primary'} onClick={() => setCapeVisibility(capeVisibility ? false : true)}>
+                            {capeVisibility ? 'Скрыть' : 'Показать'}
+                        </Button>
+                    </Panel>
+                    <Panel header="Козырёк">
+                        <CirclePicker width="auto" onChange={color => setVisorColor(color.hex)} />
+                        <Button type={visorVisibility ? 'default' : 'primary'} onClick={() => setVisorVisibility(visorVisibility ? false : true)}>
+                            {visorVisibility ? 'Скрыть' : 'Показать'}
+                        </Button>
+                    </Panel>
+                    <Panel header="Верхняя заклёпка">
+                        <CirclePicker width="auto" onChange={color => setTopPointColor(color.hex)} />
+                        <Button type={topPointVisibility ? 'default' : 'primary'} onClick={() => setTopPointVisibility(topPointVisibility ? false : true)}>
+                            {topPointVisibility ? 'Скрыть' : 'Показать'}
+                        </Button>
+                    </Panel>
+                </Collapse>
+            </Sider>
+        </Layout>
     </>
 );
 }
