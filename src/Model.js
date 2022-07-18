fbc6d0ffc76cdeef18efb8ef8ea4b34589b400eb
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-19 00:10:05 +1000

    Add collapsable menu with change/hide options for the hat parts.

--- src/Model.js
+++ src/Model.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
+import { Collapse, Button } from 'antd';
 import Hat from './Hat';
+
+const { Panel } = Collapse;
 
 export default function Model() {
 const [crownColor, setCrownColor] = useState('#FF0000');
@@ -17,6 +20,28 @@ const [topPointVisibility, setTopPointVisibility] = useState(true);
 return (
     <>
         <h1>Model</h1>
+        <Collapse>
+            <Panel header="Стенка">
+                <Button type={crownVisibility ? 'default' : 'primary'} onClick={() => setCrownVisibility(crownVisibility ? false : true)}>
+                    {crownVisibility ? 'Скрыть' : 'Показать'}
+                </Button>
+            </Panel>
+            <Panel header="Отлетная часть">
+                <Button type={capeVisibility ? 'default' : 'primary'} onClick={() => setCapeVisibility(capeVisibility ? false : true)}>
+                    {capeVisibility ? 'Скрыть' : 'Показать'}
+                </Button>
+            </Panel>
+            <Panel header="Козырёк">
+                <Button type={visorVisibility ? 'default' : 'primary'} onClick={() => setVisorVisibility(visorVisibility ? false : true)}>
+                    {visorVisibility ? 'Скрыть' : 'Показать'}
+                </Button>
+            </Panel>
+            <Panel header="Верхняя заклёпка">
+                <Button type={topPointVisibility ? 'default' : 'primary'} onClick={() => setTopPointVisibility(topPointVisibility ? false : true)}>
+                    {topPointVisibility ? 'Скрыть' : 'Показать'}
+                </Button>
+            </Panel>
+        </Collapse>
         <Canvas>
             <OrbitControls />
             <ambientLight intensity={0.5} />
