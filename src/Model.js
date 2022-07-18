cb1338d976e3b56c65502146affc2a65589f171c
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-18 22:03:25 +1000

    Add a hat model to models page.
    Install dependencies for using Three.js inside React app. Create a JSX
    component from GLB model using `npx gltfjsx <file>`. Place the component
    on Models page into @react-three/fiber Canvas along with some lights.

--- src/Model.js
+++ src/Model.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
+import { Canvas } from '@react-three/fiber'
+import Hat from './Hat';
 
 export default function Model() {
     return (
-        <h1>Model</h1>
+        <>
+            <h1>Model</h1>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <Hat />
+            </Canvas>
+        </>
     );
 }
